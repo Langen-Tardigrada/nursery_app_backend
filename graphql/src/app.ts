d4f6976881fa2d11b84c535ca77d33210041ba7c
Langen-Tardigrada <surting.st@gmail.com>
2022-08-03 12:52:08 +0700

    delete some code that are not important

--- graphql/src/app.ts
+++ graphql/src/app.ts
@@ -70,8 +70,6 @@ for (let i of ind){
     }
 }
 
-console.log(employee)
-
 const AddressType = new GraphQLObjectType({
     name: 'Address',
     fields: {
@@ -97,12 +95,6 @@ const typeDefs = gql`
         employees(query: String): [Employee]!
         accounts:[Account]!
         department:[Department]!
-        # employees: [User!]!
-        # customers: [Customer!]!
-        # kids: [Kid!]!
-        # account:[Account!]!
-        # department:[Department!]!
-        # parent:Parent!
     }
 
     type Mutation {
@@ -153,240 +145,14 @@ const typeDefs = gql`
         password: String!
         owner: User!
     }
-   
-    # type Allergy {
-    #     food: [String]!
-    #     drug: [String]!
-    # }
-    # type Customer {
-    #     user: User!
-    #     kid:Kid!
-    #     status: Boolean!
-    #     email:String!
-    #     account:Account!
-    # }
-
-    # type Kid {
-    #     kid_info: User!
-    #     nickname: String!
-    #     weight: Float!
-    #     height: Float!
-    #     insurance: String!
-    #     emergency_hosipital: String!
-    #     vaccine: [String!]!
-    #     allergy:Allergy!
-    # }
 
     type Department{
         id:ID!
      }
-    # type Parent{
-    #     parent_info: [Customer!]!
-    #     parent_relative: String!
-    # }
-
-    # type Employee{
-    #     employee_info: User!
-    #     nickname: String!
-    #     email: String!
-    #     start_date: Date!
-    #     account: Account!
-    #     department: Department!
-    #}
 `
-    // type Checkin{
-    //     parent_name: String!
-    //     kid_name: String!
-    //     kid_id: ID!
-    //     employee_name: String!
-    // }
-    // type Checkout{
-    //     parent_name: String!
-    //     kid_name: String!
-    //     kid_id: ID!
-    //     employee_name: String!
-    // }
-    // type Class{
-    //     nanny:Nanny!
-    //     schedule:Schedule!
-    //     class_name:String!
-    //     id:ID!
-
-    // }
-    // type Nanny{
-    //     name: String
-    //     position: String
-    // }
-    // type Schedule{
-    //     subject:String
-    //     time:Time!
-    // }
-    // type Department{
-    //     id: ID!
-    //     name: String!
-    //     position: String!date: Date
-    //     Reason: String
-    //     School: String
-    //     kid: Kid
-    //     duty: String!
-    //     employee: Employee!
-    //     manage: Boolean!
-    //     class: Class!
-    // }
-    // type History{
-    //     date: Date!
-    //     Reason: String!
-    //     School: String!
-    //     kid: Kid!
-    // }
-    // type Health_check{
-    //     date: Date!
-    //     injure:Injure!
-    //     ate_breakfast: Boolean!
-    //     sympton: Sympton!
-    //     comment: String!
-    //     kid: Kid!
-    //     department: Deprtment!
-    // }
-    // type Injure{
-    //     eyes: Boolean!
-    //     ears: Boolean!
-    //     mouth: Boolean!
-    //     skin:Boolean!
-    //     comment: String!}
-    // type  Sympton{
-    //     runny nose: Boolean
-    //     cough: Boolean}
-    // type  Announcement{
-    //     activities: Activity!
-    //     study_board: [Subject!]!
-    //     customer: [Customer!]!
-    //     Holiday: [{
-    //             date: Date!
-    //             day: String!
-    //     }]
-    //     Food: [String!]!
-    //     afternoon break: [String!]!
-    // }
-    // type Subject{
-    //     name: String
-    //     info: [{
-    //             title: String
-    //             date: Date
-    //             class: String
-    //             time: Time
-    //     }]Object
-    // }
-    // type Leave{
-    //     kid: Kid!
-    //     date: [Date!]!
-    //     reason: [String!]!(default "")
-
-    // }
-    // type Activity{
-    //     start_date: Date
-    //     end_date: Date
-    //     info: [{
-    //             date: Date
-    //             title: String
-    //     }]Object
-
-    // }
-    // type Behavior {
-    //     kid: Kid
-    //     date: Date
-    //     hygiene: {
-    //             have a lunch: {
-    //                     much: boolean
-    //                     nomal: boolean
-    //                     less: boolean
-    //                     not eat: boolean
-    //             }
-    //             go to toilet: {
-    //                     go afternoon: Int
-    //                     go morning: Int
-    //                     not go: Int
-    //             }
-    //             drink milk:{
-    //                     much: Boolean
-    //                     less: Boolean
-    //                     milk run out: Boolean
-    //                     not drink: Boolean
-    //             }
-    //             brushing teeth: Boolean
-    //             sleep: Boolean
-    //     }
-    //     activity: {
-    //             intellectual dev: {
-    //                     writing: Boolean
-    //                     follow order: Boolean
-    //                     talk with others: Boolean
-    //             }
-    //             social dev.: {
-    //                     help others: Boolean
-    //                     kindness: Boolean
-    //                     play with others: Boolean
-    //             }
-    //             emotional & mental dev: {
-    //                     level: Int
-    //             }
-    //             phisical dev: Boolean
-    //     }
-    //     comment: String
-    //     class: Class
-    // }
-    // type Schoolrecord{
-    //     estimate:[{
-    //         subject: Subject
-    //         record: String
-    //         score: Float
-    //         date_estimate: Date
-    //         department: Department
-    //      }]
-    //     kid: Kid
-    // }
-    // type Stock{
-    //     id: Id
-    //     kid: Kid
-    //     class: Class 
-    //     uniform: {
-    //             basic: Boolean
-    //             PE: Boolean
-    //             free style: Boolean
-    //             free wed: Boolean
-    //     }
-    //     milk: number
-    //     diapers: number
-    //     towel: boolean
-    //     water: boolean
-    // }`
-    
-
-
-// Resolvers
-// const index = Object(userInfo)
-
-// const dateScalar = new GraphQLScalarType({
-//     name: 'Date',
-//     description: 'Date custom scalar type',
-//     serialize(value: unknown) {
-//         let date:Date = new Date(value)
-//         return date.getTime(); // Convsert outgoing Date to integer for JSON
-//     },
-//     parseValue(value:unknown) {
-//         return new Date(value); // Convert incoming integer to Date
-//     },
-//     parseLiteral(ast) {
-//         if (ast.kind === Kind.INT) {
-//         return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
-//         }
-//         return null; // Invalid hard-coded value (not an integer)
-//     },
-// })
 
 //*Resolvers
 const resolvers = {
-    // Date: dateScalar,
     Date: DateScalar,
     Time: TimeScalar,
     DateTime: DateTimeScalar,
@@ -400,93 +166,9 @@ const resolvers = {
                 return user.firstname.toLowerCase().includes(args.query.toLowerCase())
             })
         },
-        // address(parent, args, ctx, info) {
-        //     if (!args.query) {
-        //         return users.
-        //     }
-        
-        // },
-        // account(parent, args, ctx, info) {
-        //     if (!args.query) {
-        //         return account
-        //     }},
-
         employees(parent,args,ctx,info) {        
             return employee
         },
-
-        // customers:(parent, args, ctx, info)=>{
-        //     let cus: any[] = []
-        //     for( let i in index){
-        //         if(userInfo[i].identity === 'customer') {
-        //             cus.push({
-        //                 user: {
-        //                     id: userInfo[i].id,
-        //                     firstname:userInfo[i].firstname,
-        //                     lastname:userInfo[i].lastname,
-        //                     email: userInfo[i].email,
-        //                     age: userInfo[i].age,
-        //                     date_of_birth:userInfo[i].date_of_birth,
-        //                     address:userInfo[i].address,
-        //                     ethnicity:userInfo[i].ethnicity,
-        //                     gender:userInfo[i].gender,
-        //                     national: userInfo[i].national,
-        //                     phone:userInfo[i].phone,
-        //                     region:userInfo[i].region,
-        //                     username:userInfo[i].username,
-        //                     password:userInfo[i].password,
-        //                 },
-        //                 kid_id:userInfo[i].kid_id,
-        //                 kid_nickname:userInfo[i].kid_nickname,
-        //                 kid_relative:userInfo[i].relative,
-        //                 status:userInfo[i].status
-        //             })
-        //         }
-        //     }
-
-        //     return cus
-        // },
-
-        // kids: () => {
-        //     let kids: any[] = []
-        //     for( let i in index){
-        //         if(userInfo[i].identity === 'student') {
-        //             kids.push({
-        //                 user: {
-        //                     id: userInfo[i].id,
-        //                     firstname:userInfo[i].firstname,
-        //                     lastname:userInfo[i].lastname,
-        //                     email: userInfo[i].email,
-        //                     age: userInfo[i].age,
-        //                     date_of_birth:userInfo[i].date_of_birth,
-        //                     address:userInfo[i].address,
-        //                     ethnicity:userInfo[i].ethnicity,
-        //                     gender:userInfo[i].gender,
-        //                     national: userInfo[i].national,
-        //                     phone:userInfo[i].phone,
-        //                     region:userInfo[i].region,
-        //                     username:userInfo[i].username,
-        //                     password:userInfo[i].password,
-
-        //                 },
-
-        //                 parent_id: userInfo[i].parent_id,
-        //                 parent_relative: userInfo[i].relative,
-        //                 nickname: userInfo[i].nickname,
-        //                 class: userInfo[i].class,
-        //                 weight: userInfo[i].weight,
-        //                 height: userInfo[i].height,
-        //                 emergency_hospital: userInfo[i].emergency_hospital,
-        //                 vaccine: userInfo[i].vaccine,
-        //                 allergy: userInfo[i].allergy,
-        //                 status: userInfo[i].status
-        //             })
-        //         }
-        //     }
-        //     console.log(kids)
-        //     return kids
-        // }
-        
     },
     Mutation: {
         createUser(parent, args, ctx, info){
